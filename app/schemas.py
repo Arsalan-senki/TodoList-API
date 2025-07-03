92046c7cc562704158fabe2a749769dea4bbfaae
Arsalan Marvi <marviarsalan@gmail.com>
2025-07-03 19:32:47 +0330

    Update schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+
+class BaseTaskClass(BaseModel):
+    title: str
+    description: Optional[str] = None
+    is_completed: Optional[bool] = False
+
+
+class TaskCreate(BaseTaskClass):
+    pass
+
+
+class TaskUpdate(BaseTaskClass):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    is_completed: Optional[bool] = None
+
+
+class TaskInDB(BaseTaskClass):
+    id: int
+    created_at: datetime
+
+    class config:
+        orm_more = True
